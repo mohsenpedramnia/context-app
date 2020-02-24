b97d6b56b6ed01a86479f4d1773aa4d4d41b2fed
Mohsen Pedramnia <mohsen.pedramnia@gmail.com>
2020-02-24 12:43:33 +0300

    Updating context data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
-
+import ThemeToggle from "./components/ThemeToggle";
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
